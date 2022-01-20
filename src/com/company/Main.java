86796ca997d6e70afbedb08a3bbd42d9cbb5a42a
mak1115 <mak1115@swcsd.us>
2022-01-20 14:10:16 -0500

    added notes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,41 @@
 package com.company;
 
+import java.util.Locale;
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
         System.out.println("Hello world!");
+
+        int age = 17;
+        double gpa = 3.2;
+        boolean isSnowy = true;
+        String city = "Columbus";
+
+        System.out.println(age +  " | " + gpa + " | " + isSnowy);
+        System.out.println(city);
+        System.out.println(city.toUpperCase());
+
+        float pi = 3.14f;
+        System.out.println(pi);
+
+        int[] dailyHighs = {50,60,70,80,90};
+
+
+
+        double quotient = 10.0/4;
+        System.out.println(quotient);
+
+        int i = 1;
+
+        int result = ++i * 2;
+        System.out.println(result);
+
+        System.out.println("What is your name? ");
+        String name = input.nextLine();
+        System.out.println(name.toUpperCase());
     }
 }
